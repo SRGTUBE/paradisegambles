350683ca9eef490ec0e486081cbcf907a85d0055
SRGTUBE <161336524+SRGTUBE@users.noreply.github.com>
2025-03-15 12:35:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=".", intents=intents, help_command=None)
+
 
 # Database setup
 conn = sqlite3.connect("points.db")
